5aa5ba2175d25a5301a6ce8e90e231a3e313c8bf
Derek Schultz <derek@derekschultz.net>
2014-08-21 14:30:49 +0000

    Change copyright

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'chronology'
-copyright = u'2014, GoDaddy Operating Company, LLC.'
+copyright = u'2014, GoDaddy Operating Company, LLC'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
