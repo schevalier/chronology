4763c22d6d6c19de7b798285dd0d08f5632b751a
Joshua Blum <joshblum@mit.edu>
2014-09-07 00:50:23 -0400

    Get, Delete, GetStreams

--- kronos.js/kronos.js
+++ kronos.js/kronos.js
@@ -6,20 +6,17 @@
 var request;
 try {
     request = require("request");
-} catch (err) {
-    request = null;
-}
+} catch (err) {}
 
 function KronosClient(options) {
     // `kronosUrl`: The Url of the Kronos server to talk to.
     // `namespace` (optional): Namespace to store events in.
-    // `debug` (optional): Log error messages to console?
     var self = this;
     options = options || {};
     var namespace = options.namespace || null;
     var $ = options.jQuery;
-    var debug = options.debug || false;
     var noop = function() {};
+    var HTTP_OK = 200;
 
     self.ID_FIELD = "@id";
     self.TIMESTAMP_FIELD = "@timestamp";
@@ -95,14 +92,23 @@ function KronosClient(options) {
     };
 
     var http = (function() {
-
-        var makeRequest = function(url, type, data, onSuccess, onError) {
+        /*
+         * Wrap the node.js `request` module, jQuery `$.ajax` and `xdr` http functionality into a single interface.
+         * Client calls use whichever library is available to execute the client logic.
+         *
+         * @param url {String}
+         * @param type{String} HTTP verb ("GET", "POST", "DELETE", etc.)
+         * @param data {String} request body
+         * @param onSuccess {Function} to callback on completion
+         * @param onError {Function} to callback on error
+         */
+        var makeRequest = function(url, method, data, onSuccess, onError) {
             onSuccess = onSuccess || noop;
             onError = onError || noop;
             if (typeof request !== "undefined") { //node.js
                 request({
                         "url": url,
-                        "method": type,
+                        "method": method,
                         "body": data,
                         "headers": {
                             "Content-type": "application/json"
@@ -117,7 +123,7 @@ function KronosClient(options) {
                     });
             } else if (typeof $ !== "undefined") { //jQuery
                 $.ajax({
-                    "type": type,
+                    "type": method,
                     "url": url,
                     "data": data,
                     "dataType": "json",
@@ -129,7 +135,7 @@ function KronosClient(options) {
                     }
                 });
             } else { // xdr AJAX request
-                xdr(url, type, data, onSuccess, onError);
+                xdr(url, method, data, onSuccess, onError);
             }
         };
 
@@ -146,24 +152,88 @@ function KronosClient(options) {
         };
     }());
 
+    /*
+     * Parse the body of a HTTP Response into json.
+     * Returns an object with any errors that occured during parsing or the
+     * parsed body
+     * @param body {String}
+     */
+    var parseJSON = function(body) {
+        var err, responseData;
+        try {
+            body = body.trim().replace("\r", "").split("\n");
+            responseData = [];
+            var object;
+            for (var i = 0; i < body.length; i++) {
+                object = body[i];
+                if (object !== "") {
+                    responseData.push(JSON.parse(object));
+                }
+            }
+        } catch (e) {
+            err = new Error("Error parsing JSON data: " + e.message);
+        }
+        return {
+            "err": err,
+            "data": responseData,
+        };
+    };
+
+    /*
+     * Returns whether or not the given `statusCode` is equal to `HTTP_OK`
+     * @param statusCode {int}
+     */
+    var httpSuccess = function(statusCode) {
+        return (statusCode === HTTP_OK);
+    };
+
+    /*
+     * Returns a new `Error` object with a message and `statusCode`
+     * @param statusCode {int}
+     */
+    var getServerError = function(statusCode) {
+        return new Error("Error contacting KronosServer, statusCode: " + statusCode);
+    };
+
+    /*
+     * Parse a response from a http request into json and send the results to the `callback` function.
+     * `errBack` can be called if the http `statusCode` is not OK, no JSON results are returned or the `SUCCESS_FIELD`
+     * is not truthy.
+     * @param body {String}
+     * @param statusCode{int}
+     * @param callback {Function} to callback on completion
+     * @param errBack {Function} to callback on error
+     */
     var parseKronosResponse = function(body, statusCode, callback, errBack) {
-        if (statusCode !== 200) {
-            errBack(new Error("Error contacting KronosServer, statusCode: " + statusCode));
+        if (!httpSuccess(statusCode)) {
+            errBack(getServerError(statusCode));
             return;
         }
-        var data;
-        try {
-            data = JSON.parse(body);
-        } catch (err) {
-            errBack(new Error("Error parsing JSON data: " + err.message));
-            return;
+        var responseData = parseJSON(body);
+        var data = responseData.data[0];
+        if (responseData.err) {
+            errBack(responseData.err);
         }
         // Check if there was a server side error?
-        if (data[self.SUCCESS_FIELD]) {
+        else if (data[self.SUCCESS_FIELD]) {
             callback(data);
         } else {
-            errBack(new Error("Error processing request: " + data));
+            errBack(new Error("Error processing request: " + responseData.data));
         }
+    };
+    /*
+     * Parse a string or Date into kronos time.
+     * @param time {String || Date || int}
+     */
+    var parseTime = function(time) {
+
+        if (typeof time === "string") {
+            time = Date.parse(time);
+        }
+        if (time instanceof Date) {
+            time = self.timeToKronosTime(time);
+        }
+        return time;
     };
 
     self.index = function(callback, errBack) {
@@ -173,10 +243,14 @@ function KronosClient(options) {
             }, errBack);
     };
 
+    /*
+     * @param stream {String} Stream to put the event in.
+     * @param event {Object} The event object.
+     * @param namespace {String} (optional): Namespace for the stream.
+     * @param callback {Function} to callback on completion
+     * @param errBack {Function} to callback on error
+     */
     self.put = function(stream, event, namespace, callback, errBack) {
-        // `stream`: Stream to put the event in.
-        // `event`: The event object.
-        // `namespace` (optional): Namespace for the stream.
         event = event || {};
         if (event[self.TIMESTAMP_FIELD] === null) {
             event[self.TIMESTAMP_FIELD] = self.kronosTimeNow();
@@ -189,6 +263,137 @@ function KronosClient(options) {
         data = JSON.stringify(data);
         http.post(putUrl, data, function(body, statusCode) {
             parseKronosResponse(body, statusCode, callback, errBack);
+        }, errBack);
+    };
+
+    /*
+     * Queries a stream with name `stream` for all events between `startTime` and
+     * `endTime` (both inclusive).  An optional `startId` allows the client to
+     * restart from a failure, specifying the last ID they read; all events that
+     * happened after that ID will be returned. An optional `limit` limits the
+     * maximum number of events returned.  An optional `order` requests results in
+     * `ASCENDING_ORDER` or `DESCENDING_ORDER`.
+     */
+    self.get = function(stream, startTime, endTime, options, callback, errBack) {
+        options = options || {};
+        startTime = parseTime(startTime);
+        endTime = parseTime(endTime);
+        var order = options.order || self.ASCENDING_ORDER;
+        var requestDict = {
+            "stream": stream,
+            "end_time": endTime,
+            "order": order,
+        };
+
+        if (options.startId) {
+            requestDict.start_id = options.startId;
+        } else {
+            requestDict.start_time = startTime;
+        }
+
+        if (options.limit) {
+            requestDict.limit = options.limit;
+        }
+        var namespace = options.namespace || self.namespace;
+        if (namespace) {
+            requestDict.namespace = namespace;
+        }
+
+        var errorCount = 0;
+        var maxErrors = 10;
+
+        var retry = function(errorCount, requestDict, callback, errBack) {
+            callback = callback || noop;
+            errBack = errBack || noop;
+            var err;
+            var data = JSON.stringify(requestDict);
+
+            // TODO(jblum): stream response
+            http.post(getUrl, data, function(body, statusCode) {
+                if (!httpSuccess(statusCode)) {
+                    errorCount++;
+                    err = getServerError(statusCode);
+                } else {
+                    var responseData = parseJSON(body);
+                    if (responseData.err) {
+                        errorCount++;
+                        err = responseData.err;
+                    }
+                    if (errorCount === maxErrors) {
+                        errBack(err);
+                    } else if (err) {
+                        retry(errorCount, requestDict, callback, errBack);
+                    } else {
+                        callback(responseData.data);
+                    }
+                }
+            }, function(err) {
+                errorCount++;
+                if (errorCount === maxErrors) {
+                    errBack(err);
+                } else if (err) {
+                    retry(errorCount, requestDict, callback, errBack);
+                }
+            });
+        };
+        retry(errorCount, requestDict, callback, errBack);
+    };
+
+    /*
+     * Delete events in the stream with name `stream` that occurred between
+     * `start_time` and `end_time` (both inclusive).  An optional `startId` allows
+     * the client to delete events starting from after an ID rather than starting
+     * at a timestamp.
+     */
+    self.delete = function(stream, startTime, endTime, options, callback, errBack) {
+        options = options || {};
+        startTime = parseTime(startTime);
+        endTime = parseTime(endTime);
+        var requestDict = {
+            "stream": stream,
+            "end_time": endTime,
+        };
+
+        if (options.startId) {
+            requestDict.start_id = options.startId;
+        } else {
+            requestDict.start_time = startTime;
+        }
+
+        var namespace = options.namespace || self.namespace;
+        if (namespace) {
+            requestDict.namespace = namespace;
+        }
+
+        http.post(deleteUrl, JSON.stringify(requestDict),
+            function(body, statusCode) {
+                parseKronosResponse(body, statusCode, callback, errBack);
+            }, errBack);
+    };
+
+    /*
+     * Queries the Kronos server and fetches a list
+     * of streams available to be read.
+     */
+    self.get_streams = function(options, callback, errBack) {
+        options = options || {};
+        var requestDict = {};
+        var namespace = options.namespace || self.namespace;
+        if (namespace) {
+            requestDict.namespace = namespace;
+        }
+        var data = JSON.stringify(requestDict);
+        http.post(streamsUrl, JSON.stringify(requestDict), function(body, statusCode) {
+            if (!httpSuccess(statusCode)) {
+                errBack(getServerError(statusCode));
+            } else {
+                var responseData = parseJSON(body);
+                if (responseData.err) {
+                    errBack(responseData.err);
+                } else {
+                    callback(responseData.data);
+                }
+            }
         }, errBack);
     };
     return self;
