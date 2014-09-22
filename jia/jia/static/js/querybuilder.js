60302dd41fc43102a1a64dfb67b20d51e267951c
Derek Schultz <derek@derekschultz.net>
2014-09-22 16:07:05 +0000

    Flow schema through query builder. Fix #129.

--- jia/jia/static/js/querybuilder.js
+++ jia/jia/static/js/querybuilder.js
@@ -48,8 +48,28 @@
  *   field: <cpf>,
  *   alias: <string>
  * }
+ *
+ *
+ * Schemas
+ * -------
+ * Keeping track of the schema is necessary for offering autocomplete on
+ * property names. The schema must be flowed through the query, as each step
+ * in the query has the ability to modify the schema for future steps. For
+ * example, a transform operation adds a new property to the schema.
+ *
+ * Schemas are stored as object keys to emulate the behavior of sets.
+ *
+ * sample_schema = {"property1": true, "property2": true};
+ *
+ * `schemas[0]` is fed by `panel.cache.streamProperties`. The schema is then
+ * flowed through all the steps of the query. Each step takes in
+ * `schemas[index]`, makes modifications if necessary, and outputs results in
+ * `schemas[index + 1]`. Because each step `$watch`es its input, and each
+ * step's input is the previous step's output, a change anywhere will
+ * immediately flow through all the following steps.
  * 
  */
+
 
 
 var qb = angular.module('jia.querybuilder', []);
@@ -62,14 +82,31 @@ function findObjectInListBasedOnKey(list, keyName, keyVal) {
   }
 }
 
-qb.directive('querybuilder', function ($http, $compile) {
+qb.directive('querybuilder', function () {
   var controller = ['$scope', function($scope) {
     $scope.nextStep = {};
     $scope.query = $scope.panel.data_source.query;
 
+    // Provide an empty schema set for `schemas[0]` as a placeholder until the
+    // AJAX call to `infer_schema` returns a result.
+    $scope.schemas = [{}];
+
+    // Allow access from other places (like settings panel)
+    $scope.panel.cache.schemas = $scope.schemas;
+
+    $scope.$watch('panel.cache.streamProperties', function (newVal, oldVal) {
+      if (newVal) {
+        $scope.schemas[0] = {};
+        for (var i = 0; i < newVal.length; i++) {
+          $scope.schemas[0][newVal[i]] = true;
+        }
+      }
+    });
+
     $scope.$watch('nextStep.operation', function (newVal, oldVal) {
       if (newVal && !_.isEmpty(newVal)) {
         var newStep = angular.copy($scope.nextStep);
+
         $scope.query.steps.push(newStep);
         $scope.nextStep = {};
       }
@@ -94,6 +131,16 @@ qb.directive('querybuilder', function ($http, $compile) {
 });
 
 qb.directive('step', function ($http, $compile) {
+  /*
+   * Single query step
+   * 
+   * :param step: Reference to step model
+   * :param schemas: Reference to schemas list for query (schema is different
+   * at each step)
+   * :param index: Zero-based index of this step in the query
+   * :param newop: Boolean specifying if this is an active query step or a
+   * "New Operation..." select element.
+   */
   var linker = function (scope, element, attrs) {
     scope.$watch(function () {
       return scope.step.operation;
@@ -143,12 +190,81 @@ qb.directive('step', function ($http, $compile) {
       }
     ];
 
+    // Keep track of all new fields created during this step
+    $scope.step.fields = [];
+
+    if (typeof $scope.schemas != 'undefined') {
+      $scope.schemaIn = $scope.schemas[$scope.index];
+      $scope.schemas[$scope.index + 1] = {}; 
+
+      // Changes to schemaIn require an update to the output schema in index+1
+      $scope.$watch('schemaIn', function (newVal, oldVal) {
+        $scope.schemaIn = newVal;
+        $scope.updateSchema();
+      });
+
+      // Any change to fields created in this query step requires an update
+      $scope.$watch('step.fields', function () {
+        $scope.updateSchema();
+      }, true);
+    }
+
     $scope.addOperand = function (operands) {
       operands.push({});
     };
+
     $scope.removeOperand = function (idx, operands) {
       operands.splice(idx, 1);
     }
+
+    $scope.replaceSchema = function () {
+      /*
+       * Create ouptut schema set containing only the new properties defined in
+       * this query step
+       */
+      var schemaOut = {};
+      for (var field in $scope.step.fields) {
+        schemaOut[$scope.step.fields[field]] = true;
+      }
+      $scope.schemas[$scope.index + 1] = schemaOut;
+    };
+
+    $scope.mergeSchema = function () {
+      /*
+       * Create output schema set containing the properties in the input schema
+       * set in addition to the new properties defined in this query step.
+       */
+      var schemaOut = angular.extend({}, $scope.schemaIn);
+      for (var field in $scope.step.fields) {
+        schemaOut[$scope.step.fields[field]] = true;
+      }
+      $scope.schemas[$scope.index + 1] = schemaOut;
+    };
+    
+    $scope.updateSchema = function () {
+      /*
+       * Select schema transform type based on the type of operation being
+       * performed in this step.
+       */
+      if ($scope.step.operation.operator == 'aggregate') {
+        $scope.replaceSchema();
+      }
+      else if ($scope.step.operation.operator == 'transform') {
+        $scope.mergeSchema();
+      }
+      else {
+        $scope.schemas[$scope.index + 1] = $scope.schemaIn;
+      }
+    };
+
+    // If the operation type changes, the output schema needs an update
+    $scope.$watch(function () {
+      return $scope.step.operation;
+    }, function (newVal, oldVal) {
+      if (newVal) {
+        $scope.updateSchema();
+      }
+    });
   }];
 
   return {
@@ -158,13 +274,22 @@ qb.directive('step', function ($http, $compile) {
     link: linker,
     scope: {
       step: '=',
+      schemas: '=',
+      index: '=',
       newop: '='
     }
   };
 });
 
-
-qb.directive('cpf', function ($http, $compile) {
+qb.directive('cpf', function () {
+  /*
+   * Constant/property/function
+   *
+   * See top of file for detailed information on the structure of the CPF
+   * model.
+   *
+   * :param arg: CPF model
+   */
   var controller = ['$scope', function ($scope) {
     $scope.functions = [
       {
@@ -261,7 +386,7 @@ qb.directive('cpf', function ($http, $compile) {
     ];
     $scope.type = $scope.types[0];
     $scope.args = [];
-        
+    
     if (!$scope.arg || $scope.arg.property_only) {
       $scope.arg = {};
     }
@@ -318,12 +443,17 @@ qb.directive('cpf', function ($http, $compile) {
     templateUrl: '/static/partials/operators/cpf.html',
     controller: controller,
     scope: {
-      arg: '=' 
+      arg: '='
     }
   };
 });
 
-qb.directive('op', function ($http, $compile) {
+qb.directive('op', function () {
+  /*
+   * Basic filter operator type select element
+   *
+   * :param arg: Model to store operator
+   */
   var linker = function (scope, element, attrs) {
     if (scope.arg) {
       var type = scope.arg;
@@ -366,7 +496,12 @@ qb.directive('op', function ($http, $compile) {
   };
 });
 
-qb.directive('aggtype', function ($http, $compile) {
+qb.directive('aggtype', function () {
+  /*
+   * Provides a dropdown list of aggregation types
+   *
+   * :param arg: Model to store selected value
+   */
   var linker = function (scope, element, attrs) {
     if (scope.arg) {
       var val = scope.arg;
@@ -407,14 +542,32 @@ qb.directive('aggtype', function ($http, $compile) {
   };
 });
 
-qb.directive('value', function ($http, $compile) {
+qb.directive('value', function () {
+  /*
+   * Thin wrapper around <input>
+   *
+   * :param arg: Model to update with <input> contents
+   * :param altersSchema: (optional) Contains a reference to a query `step` if
+   * the directive is being used to add a property to the schema.
+   * :param placeholder: (optional) Placeholder text for <input>
+   * :param type: (optional) TODO(derek): Not implemented
+   *
+   */
   var linker = function (scope, element, attrs) {
     scope.val = scope.arg;
   };
-
+ 
+  // Behaves statically because a directive's factory function is invoked only
+  // once, when the compiler matches the directive for the first time.
+  var id = 0;
   var controller = ['$scope', function ($scope) {
+    // Get unique ID from incrementing static `id`
+    var uid = id++;
     $scope.$watch('val', function () {
       $scope.arg = $scope.val;
+      if ($scope.altersSchema) {
+        $scope.altersSchema.fields[uid] = $scope.val;
+      }
     });
 
     if (!$scope.arg) {
@@ -429,13 +582,19 @@ qb.directive('value', function ($http, $compile) {
     link: linker,
     scope: {
       arg: '=',
+      altersSchema: '=?',
       placeholder: '=?',
       type: '=?'
     }
   };
 });
 
-qb.directive('direction', function ($http, $compile) {
+qb.directive('direction', function () {
+  /*
+   * Sort order (asc/desc) select element
+   *
+   * :param arg: Model to store direction
+   */
   var linker = function (scope, element, attrs) {
     scope.direction = scope.arg;
   };
@@ -464,4 +623,27 @@ qb.directive('direction', function ($http, $compile) {
       arg: '='
     }
   };
+});
+
+qb.directive('property', function () {
+  /*
+   * Autocomplete property name field
+   *
+   * :param model: Model to store property name in
+   * :param schema: Schema set to extract keys from
+   */
+  var controller = ['$scope', function ($scope) {
+    $scope.$watch('schema', function (newVal, oldVal) {
+      $scope.properties = Object.keys($scope.schema);
+    });
+  }];
+  return {
+    restrict: 'E',
+    templateUrl: '/static/partials/operators/property.html',
+    controller: controller,
+    scope: {
+      model: '=',
+      schema: '='
+    }
+  }
 });
